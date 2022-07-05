e48023956ee6b3a666bbf58b6d93bfb236d7cacf
tayefulislam <kazitayefulislam12@gmail.com>
2022-07-05 18:04:05 +0600

    Return the first element in an array

---
+++ easyproblems.js
@@ -0,0 +1,28 @@
+// Return the First Element in an Array
+
+const arr = []
+
+function reFisrtElement(arr){
+
+    if(arr.length ===0){
+
+        return "Empty Aarry"
+
+    }
+
+    else{
+
+        return arr[0]
+    }
+
+
+}
+
+console.log(reFisrtElement(arr))
+
+
+
+// https://www.educative.io/blog/level-up-javascript-coding-challenges
+
+
+
